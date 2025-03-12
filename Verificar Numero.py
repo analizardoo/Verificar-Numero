784756afc323e5949302523b4ca8416f06626246
analizardoo <analizardo50@gmail.com>
2025-03-12 18:53:02 -0300

    Add files via upload
    Programa que tem como finalidade verificar se um numero, que quando inserido pelo usuário, analisa se é maior, menor ou igual a zero. Realizado através da linguagem Python e produzido pela plataforma Visual Studio Code.

---
+++ Verificar Numero.py
@@ -0,0 +1,7 @@
+numero = float (input("Digite um numero: "))
+if numero < 0:
+    print("numero menor que 0")
+elif numero == 0:
+    print("numero é igual a 0")
+else:
+    print ("numero é maior que 0")
